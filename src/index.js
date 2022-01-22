d8bfcd37f5867d36f83d089a61e491b9f71912e8
Handrei Fontana <95292655+HandreiFontana@users.noreply.github.com>
2022-01-22 20:15:33 -0300

    feat(account): Created new account function with username unique

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.send('Hello World!');
-})
+app.use(express.json())
+
+const users = [];
+
+app.post("/users", (request, response) => {
+    const { name, username } = request.body;
+
+    const usernameAlreadyExists = users.some((user) => user.username === username)
+
+    if (usernameAlreadyExists) {
+        return response.status(400).json({ Error: "Username already exists." })
+    }
+
+    const createdUser = {
+        id: uuidv4(),
+        name,
+        username,
+        todos: []
+    };
+
+    users.push(createdUser);
+
+    return response.status(201).send(createdUser)
+});
 
 app.listen(9090);
